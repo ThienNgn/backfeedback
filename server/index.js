f9e47f3e8661daf2d5e4bb62ed067002a7aeb83c
Thien Nguyen <thienandcode@gmail.com>
2017-12-26 21:59:43 +0100

    Update node versions and deployment from root

--- server/index.js
+++ server/index.js
@@ -6,4 +6,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});
