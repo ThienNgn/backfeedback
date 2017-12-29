6e120a8a409d111b6c3728ef77ca4b209bf20527
Thien Nguyen <thienandcode@gmail.com>
2017-12-29 19:06:00 +0100

    Add credits to user, middleware check if logged in

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,13 +1,20 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       curreny: 'usd',
       description: '$5 for 5 credits',
       source: req.body.id
     });
+
+    // Update amount of credits after transaction
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
