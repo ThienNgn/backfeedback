924ef67e61fb486eed1b1ecced5d1c9e3c06319e
Thien Nguyen <thienandcode@gmail.com>
2017-12-29 16:45:14 +0100

    Add payment component linked to Stripe

---
+++ client/src/components/Payments.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name={'Backfeedback'}
+        description={'$5 for 5 email credits'}
+        amount={500}
+        token={token}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payments;
